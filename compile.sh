04d7567cc167c301d2cebc93936197947b5e1d1a
khahliso-kgodumo <102834265+khahliso-kgodumo@users.noreply.github.com>
2026-02-22 18:54:41 +0200

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,9 +1,8 @@
-
-g++ -std=c++11 -Wall -Wextra -g main.cpp -o practical-1.cgi
-sudo chmod +x practical-1.cgi
-
+g++ -std=c++11 -Wall -Wextra -g main.cpp -o practical-1
+sudo chmod +x practical-1
+echo "md5 hash : $(md5sum practical-1)"
 # delete files
-sudo rm /usr/lib/cgi-bin/practical-1.cgi
+sudo rm /usr/lib/cgi-bin/practical-1
 sudo rm /var/www/html/cos332-html/wrong.htm
 sudo rm /var/www/html/cos332-html/right.htm
 sudo rm /var/www/html/cos332-css/beautiful.css
@@ -12,4 +11,4 @@ sudo rm /var/www/html/cos332-css/beautiful.css
 sudo cp wrong.htm /var/www/html/cos332-html/
 sudo cp right.htm /var/www/html/cos332-html/
 sudo cp beautiful.css /var/www/html/cos332-css
-sudo mv practical-1.cgi /usr/lib/cgi-bin
+sudo mv practical-1 /usr/lib/cgi-bin
